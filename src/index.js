5b8cccac1f0fc5133256ce6360b0a7b99e606ff7
sathishkumarmsk <sathishkumarmsk5699@gmail.com>
2022-01-20 17:25:23 +0530

    user register

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+
+import db from './models';
+
+
+import userRouter from './routes/user';
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use('/user', userRouter);
+
+app.use('/', (req, res) => res.send('Hello '));
+
+const server = http.createServer(app);
+
+db.sequelize.sync().then(() => {
+    server.listen(port, () => console.log(`Running on http://localhost:${port}`));
+});
---
+++ src/routes/user.js
@@ -0,0 +1,57 @@
+import { Router } from "express";
+
+import bcrypt from 'bcrypt';
+
+import db from "../models";
+
+const router = Router();
+
+router.get('/', async (req, res) => {
+    const users = await db.user.findAll({
+        attributes: {
+            exclude: ['password'],
+        },
+    });
+    if (users) {
+        return res.status(200).json(users);
+    }
+    return res.sendStatus(404);
+});
+router.post('/register', async(req, res) => {
+    console.log(req.body);
+    // Check User Exist already
+    const user = await db.user.findOne({ where: { email: req.body.email } });
+    console.log('check user exist', user);
+    // Password encrypt
+    if (user === null) {
+        const encryptedPassword = bcrypt.hashSync(req.body.password, 10);
+        console.log('Hashed password', encryptedPassword);
+        // Write email, encrypted password to db
+        const createdUser = await db.user.create({
+            email: req.body.email,
+            password: encryptedPassword,
+        });
+
+        return res.status(200).json(createdUser);
+    }
+
+    return res.status(404).json({ msg: 'User already exist' });
+});
+
+router.post('/login', async(req, res) => {
+    const user = await db.user.findOne({ where: { email: req.body.email }});
+
+    if (user === null) {
+        return res.status(404).json({ msg: "Email not found"});
+    }
+
+    const convertString = JSON.stringify(user);
+    const passwordData = JSON.parse(convertString);
+    const hash = bcrypt.compareSync(req.body.password, passwordData.password);
+    if (hash === true) {
+        return res.status(200).json({ msg: "Logged in Successfully" });
+    }
+    return res.status(404).json({ msg: "Password is incorrect" });
+});
+
+export default router;
